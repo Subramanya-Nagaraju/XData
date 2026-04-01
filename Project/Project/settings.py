ebda82501b382d5f52e47fc986b576bf467bc70b
Amith <Amithgowda2210@outlook.com>
2026-04-01 22:39:41 +0530

    second commit

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -86,21 +88,32 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Project.wsgi.application'
 
+DB_LIVE = os.getenv("DB_LIVE")
+
 
 # Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'wolffkran',
-        'USER': 'root',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
-
+if DB_LIVE in ["False", False]:
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.mysql',
+                'NAME': 'wolffkran',
+                'USER': 'root',
+                'PASSWORD': 'admin',
+                'HOST': 'localhost',
+                'PORT': '3306',
+            }
+        }
+else:
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.mysql',
+                'NAME': os.getenv("DB_NAME"),
+                'USER': os.getenv("DB_USER"),
+                'PASSWORD': os.getenv("DB_PASSWORD"),
+                'HOST': os.getenv("DB_HOST"),
+                'PORT': os.getenv("DB_PORT"),
+            }
+        }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
